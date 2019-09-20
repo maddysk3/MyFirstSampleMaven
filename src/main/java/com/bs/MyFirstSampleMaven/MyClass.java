dc8cc31081cd7f5ff9707506eba159a06e15ce85
maddysk3 <mireshsonkable@gmail.com>
2019-09-20 23:01:40 +0530

    First Commit

---
+++ src/main/java/com/bs/MyFirstSampleMaven/MyClass.java
@@ -0,0 +1,13 @@
+package com.bs.MyFirstSampleMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyClass 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
